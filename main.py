314d935bc2a18d86476c8d70f00d931f99570c39
Nelson Malbone <nhmalbone@icloud.com>
2024-05-28 18:32:24 -0400

    student project App 8 got files into a list.

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import streamlit as st
 import glob
+from pathlib import Path
 import plotly.express as px
 import regex as re
-from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 def main():
 
-
+    corpus = []
     # importing data using glob
     filepaths = glob.glob('diary/*.txt')
+
+
+    for nr, filepath in enumerate(filepaths):
+        with open(filepath, "r") as file:
+            content = file.read()
+            corpus.append(content)
+            print(nr + 1, corpus)
+
+    # looking at the pos and neg of each file
+    analyzer = SentimentIntensityAnalyzer()
+    scores = analyzer.polarity_scores(content)
+    print()
+
+    if scores ['pos'] > scores ['neg']:
+        print("its positive")
+    else:
+        print("its negative")
+
+
+
+
+
+
 
     # setting up webpage
     st.title("Diary Tone")
