3c3e2648ca68ef6900daea625fe8e593e85fbbc1
Nelson Malbone <nhmalbone@icloud.com>
2024-05-28 18:48:01 -0400

    student project App 8 just focusiing on getting the data first then ill redo the website part

--- main.py
+++ main.py
@@ -4,45 +4,16 @@ from pathlib import Path
 import plotly.express as px
 import regex as re
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-def main():
-
-    corpus = []
-    # importing data using glob
-    filepaths = glob.glob('diary/*.txt')
-
-
-    for nr, filepath in enumerate(filepaths):
-        with open(filepath, "r") as file:
-            content = file.read()
-            corpus.append(content)
-            print(nr + 1, corpus)
-
-    # looking at the pos and neg of each file
-    analyzer = SentimentIntensityAnalyzer()
-    scores = analyzer.polarity_scores(content)
-    print()
-
-    if scores ['pos'] > scores ['neg']:
-        print("its positive")
-    else:
-        print("its negative")
 
 
 
+# importing data using glob
+filepaths = glob.glob('diary/*.txt')
+corpus = []
 
+for nr, filepath in enumerate(filepaths):
 
-
-
-    # setting up webpage
-    st.title("Diary Tone")
-
-    # Positivity Section
-    # positivity header
-    st.subheader("Positivity")
-
-    # Negativity Section
-    # negativity header
-    st.subheader("Negativity")
-
-if __name__ == "__main__":
-    main()
+    with open(filepath, "r") as file:
+        content = file.read()
+        corpus.append(content)
+print(nr + 1, corpus)
